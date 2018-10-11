a793fbc0bcd5851ba1fc87e5a466a652a4f96936
nate <nate@blend.com>
2018-10-11 15:20:52 +0800

    going with non-bitwise interpretation of cpu for now

--- src/cpu/z80.ts
+++ src/cpu/z80.ts
@@ -17,10 +17,10 @@ interface Registers {
 
   // 16-bit registers
   flags: {
-    zero: number;
-    carry: number;
-    operation: number;
-    halfCarry: number;
+    zero: boolean;
+    carry: boolean;
+    operation: boolean;
+    halfCarry: boolean;
   };
   stackPointer: number;
   clock: Clock;
@@ -43,15 +43,15 @@ export class Z80 {
       l: 0,
 
       flags: {
-        zero: 0,
-        carry: 0,
-        operation: 0,
-        halfCarry: 0,
+        zero: false,
+        carry: false,
+        operation: false,
+        halfCarry: false,
       },
 
       stackPointer: 0,
 
-      // CPU also tracks m and t, not sure why
+      // TODO (nw): why does the CPU also keep track of the clock? I'm unclear what's going on here
       clock: {
         lastInstruction: 0,
         total: 0,
@@ -61,16 +61,38 @@ export class Z80 {
 
   clearFlags() {
     this.registers.flags = {
-      zero: 0,
-      carry: 0,
-      operation: 0,
-      halfCarry: 0,
+      zero: false,
+      carry: false,
+      operation: false,
+      halfCarry: false,
     };
   }
 
+  /*
+   * Value in register 2 gets added to the value in register 1
+   * The sum is left in register1
+   */
   add(register1: EightBitRegister, register2: EightBitRegister) {
-    // Value in register 2 gets added to the value in register 1
-    // The sum is left in register1
-    this.registers[register1] = this.registers[register1] + this.registers[register2];
+    this.registers[register1] += this.registers[register2];
+    this.clearFlags();
+
+    if (this.registers[register1] % 256 === 0) {
+      this.registers.flags.zero = false;
+    }
+
+    // Z80._r.a += Z80._r.e;                      // Perform addition
+    // Z80._r.f = 0;                              // Clear flags
+
+    // if(!(Z80._r.a & 255)) Z80._r.f |= 0x80;    // Check for zero
+    // What is this doing? From the inside:
+    // bitwise & with 255. Given that it's an 8bit register, that means
+    // XXXXXXXX & 11111111 => XXXXXXXX.
+    // So this basically means (if XXXXXXX === 0). Unclear why he didn't just do that.
+    // It's basically a modulus operator
+
+
+    // if(Z80._r.a > 255) Z80._r.f |= 0x10;       // Check for carry
+    // Z80._r.a &= 255;                           // Mask to 8-bits
+    // Z80._r.m = 1; Z80._r.t = 4;                // 1 M-time taken
   }
 }
